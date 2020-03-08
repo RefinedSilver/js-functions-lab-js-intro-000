0791f42ebf37237761f322b5d6ecfe9958b37dc7
Ian McKenzie <imckenzie777@gmail.com>
2020-03-08 00:14:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}`;
+  return `Happy holidays, ${name}!`;
 }
 
 function happyCustomHolidayTo(holiday, name) {
-  return `Happy ${holiday}, ${name}`;
+  return `Happy ${holiday}, ${name}!`;
 }
 function happyCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`;
